f0212c3854d4809f94f57b57726e23fe5f8b2f8d
crismora80 <crismora80@yahoo.com>
2021-11-10 14:21:30 +0200

    314 - fixed some PEP 8 warnings

--- 314/Domain/masinaValidator.py
+++ 314/Domain/masinaValidator.py
@@ -5,7 +5,8 @@ class MasinaValidator:
     def valideaza(self, masina: Masina):
         erori = []
         if masina.nivelConfort not in ["standard", "ridicat", "premium"]:
-            erori.append("Nivelul de confort trebuie sa fie 'standard', 'ridicat', 'premium'!")
+            erori.append("Nivelul de confort trebuie sa fie "
+                         "'standard', 'ridicat', 'premium'!")
         if masina.plataCard not in ['da', 'nu']:
             erori.append("Plata card poate fi 'da' sau 'nu!")
         if len(erori) > 0:
